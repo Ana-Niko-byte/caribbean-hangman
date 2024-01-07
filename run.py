6905c36242344a9d039c46d9376906f7b4b90048
Ana-Niko-byte <ananikolayenia@gmail.com>
2024-01-07 18:27:12 +0100

    Define stages of hangman

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def hangman(word):
+    wrong = 0
+    stages = [
+        '_____________', 
+        '|            ',
+        '|     ___    ',
+        '|    //|\\   ',
+        '|  --------- ',
+        '|    (   )   ',
+        '|      |     ',
+        '|   __| |__  ',
+        '|     | |    ', 
+        '|    _| |_    '
+    ]
+    rletters = list(word)
+    return rletters
+
+print(hangman('vova'))
+
